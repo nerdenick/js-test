5c324cdca571c0e5cee606ad37cb9de524351b54
Nick Jevons <nerdenick@gmail.com>
2022-01-14 16:59:03 -0800

    following along to things I don't understand

--- RegEx/validation.js
+++ RegEx/validation.js
@@ -2,6 +2,24 @@ let reg = /[a-z]/gi;
 
 let reg2 = newRegExp(/[a-z]/,'i');
 
+//including this after telephone section
+const inputs = document.querySelectorAll('input');
+
+//telephone number
 const patterns = {
     telephone: /^[\d]{10}$/
 };
+
+//validation function
+function validate(field, regex){
+    console.log(regex.test(value));
+}
+
+//input I have no idea what it does
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) =>{
+        console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+
+});
